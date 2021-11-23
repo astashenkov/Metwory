f0885f46b6c95a93ad06a8f0e255f9a5143d7dae
astashenkov <ya.astashenkov@gmail.com>
2021-11-23 22:10:42 +0300

    Metwory v 1.01

--- Matwory.js
+++ Matwory.js
@@ -18,27 +18,29 @@ cards.forEach((item, i) => {
 });
 
 function flipCard(i) {
-    if (statusCards.dropping) {
-        cards[statusCards.card1].style.backgroundColor = '#2962ff';
-        cards[statusCards.card1].textContent = 'Opend Card';
-        cards[statusCards.card1].style.fontSize = '20px';
-        cards[statusCards.card2].style.backgroundColor = '#2962ff';
-        cards[statusCards.card2].textContent = 'Opend Card';
-        cards[statusCards.card2].style.fontSize = '20px';
-        statusCards.card1 = 'n';
-        statusCards.card2 = 'n';
-        statusCards.dropping = false;
-    }
-    (statusCards.card1 == 'n') ? statusCards.card1 = i : statusCards.card2 = i;
-    cards[i].style.backgroundColor = 'red';
-    cards[i].textContent = `${numbers[i]}`;
-    cards[i].style.fontSize = '50px';
-    if (statusCards.card2 != 'n') {
-        if (numbers[statusCards.card1] != numbers[statusCards.card2]) {
-            statusCards.dropping = true;
-        } else {
+    if (cards[i].style.backgroundColor != '#f44336') {
+        if (statusCards.dropping) {
+            cards[statusCards.card1].style.backgroundColor = '#2962ff';
+            cards[statusCards.card1].textContent = 'Opend Card';
+            cards[statusCards.card1].style.fontSize = '20px';
+            cards[statusCards.card2].style.backgroundColor = '#2962ff';
+            cards[statusCards.card2].textContent = 'Opend Card';
+            cards[statusCards.card2].style.fontSize = '20px';
             statusCards.card1 = 'n';
             statusCards.card2 = 'n';
+            statusCards.dropping = false;
+        }
+        (statusCards.card1 == 'n') ? statusCards.card1 = i : statusCards.card2 = i;
+        cards[i].style.backgroundColor = '#f44336';
+        cards[i].textContent = `${numbers[i]}`;
+        cards[i].style.fontSize = '50px';
+        if (statusCards.card2 != 'n') {
+            if (numbers[statusCards.card1] != numbers[statusCards.card2]) {
+                statusCards.dropping = true;
+            } else {
+                statusCards.card1 = 'n';
+                statusCards.card2 = 'n';
+            }
         }
     }
 }
