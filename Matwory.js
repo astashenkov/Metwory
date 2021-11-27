c36639d4c07823735c6bb6c656b967d74304cf47
astashenkov <ya.astashenkov@gmail.com>
2021-11-27 22:41:27 +0300

    Fixed a bug with reopening cards

--- Matwory.js
+++ Matwory.js
@@ -3,9 +3,9 @@
 
 'use strict'
 
-const numbers = [];
-const cards = document.querySelectorAll('button');
-let statusCards = {
+const numbers = [],
+      cards = document.querySelectorAll('button');
+const statusCards = {
     dropping: false,
     card1: 'empty',
     card2: 'empty',
@@ -18,22 +18,19 @@ cards.forEach((item, i) => {
 });
 
 function flipCard(i) {
-    if (cards[i].style.backgroundColor != '#f44336') {
+    if (cards[i].classList != 'red') {
         if (statusCards.dropping) {
-            cards[statusCards.card1].style.backgroundColor = '#2962ff';
+            cards[statusCards.card1].classList = 'blue';
             cards[statusCards.card1].textContent = 'Opend Card';
-            cards[statusCards.card1].style.fontSize = '20px';
-            cards[statusCards.card2].style.backgroundColor = '#2962ff';
+            cards[statusCards.card2].classList = 'blue';
             cards[statusCards.card2].textContent = 'Opend Card';
-            cards[statusCards.card2].style.fontSize = '20px';
             statusCards.card1 = 'empty';
             statusCards.card2 = 'empty';
             statusCards.dropping = false;
         }
-        (statusCards.card1 == 'empty') ? statusCards.card1 = i : statusCards.card2 = i;
-        cards[i].style.backgroundColor = '#f44336';
+        (statusCards.card1 === 'empty') ? statusCards.card1 = i : statusCards.card2 = i;
+        cards[i].classList = 'red';
         cards[i].textContent = `${numbers[i]}`;
-        cards[i].style.fontSize = '50px';
         if (statusCards.card2 != 'empty') {
             if (numbers[statusCards.card1] != numbers[statusCards.card2]) {
                 statusCards.dropping = true;
